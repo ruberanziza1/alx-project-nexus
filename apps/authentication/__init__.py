f0f2d68a8452adc171a6bc9a7899098a5253b093
RUBERANZIZA Janvier <janvier.ruberanziza@gmail.com>
2025-09-19 14:12:49 +0200

    feat: initialize authentication app package

---
+++ apps/authentication/__init__.py
@@ -0,0 +1,9 @@
+# apps/authentication/__init__.py
+
+"""
+Authentication App
+
+This app handles all user authentication and authorization functionality.
+"""
+
+default_app_config = 'apps.authentication.apps.AuthenticationConfig'
